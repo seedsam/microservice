f340b2aca236bea3566e38898d333dba2b51531d
魏延青 <seedsam@163.com>
2019-06-21 17:37:19 +0800

    配置中心

---
+++ config_server_8762/src/test/java/com/example/demo/ConfigServer8762ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigServer8762ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
