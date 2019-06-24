e3618be46c2b60b89a28a149ec3868ccdb905c08
魏延青 <seedsam@163.com>
2019-06-24 09:32:34 +0800

    新建模板module

---
+++ zuul_5000/src/main/java/com/example/demo/Zuul5000Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zuul5000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zuul5000Application.class, args);
+	}
+
+}
