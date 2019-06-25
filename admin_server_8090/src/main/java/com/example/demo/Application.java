2fdcf35003799ad5c512699dda42934e1b9fff6c
魏延青 <seedsam@163.com>
2019-06-25 15:33:06 +0800

    test

--- admin_server_8090/src/main/java/com/example/demo/Application.java
+++ admin_server_8090/src/main/java/com/example/demo/Application.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
